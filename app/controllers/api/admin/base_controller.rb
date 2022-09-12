5fd3f302ccc686281c51143102bab9138439ae27
Manish <manish@joharimani@gmail.com>
2022-09-12 11:55:58 +0530

    Reset session for Each API request

--- app/controllers/api/admin/base_controller.rb
+++ app/controllers/api/admin/base_controller.rb
@@ -1,4 +1,9 @@
 class Api::Admin::BaseController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
   skip_before_action :verify_authenticity_token
+
+  before_action :reset_session, :authenticate_user!
 end
